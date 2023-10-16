f397eb1228d4b7a47f5f610f2cdcb269ffbb3fb8
Leonardo Medeiros <leo-sbs@live.com>
2023-10-15 22:57:11 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -9,13 +9,34 @@ data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+       // TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+
+        if (!inscritos.contains(usuario)){
+            inscritos.add(usuario)
+            println("matricula relizada com sucesso para $usuario na formação $nome")
+        }else{
+            println("$usuario ja matriculado : $nome ")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+   // TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+
+    val usuario1 = Usuario()
+    val usuario2 = Usuario()
+
+    val conteudo1 = ConteudoEducacional("conteudo1 ")
+    val conteudo2 = ConteudoEducacional("conteudo2 ")
+
+
+    val formacao1 = Formacao("Formacação1 ", listOf(conteudo1 , conteudo2))
+
+    formacao1.matricular(usuario1)
+    formacao1.matricular(usuario2)
+    formacao1.matricular(usuario1)
 }
